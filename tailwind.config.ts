fbbdad449bef8ee9cfa31a1c6e50a540cc671745
mini-1018 <anjaemin1018@gmail.com>
2025-01-17 16:22:18 +0900

    :sparkles: [feat] 이미지

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,31 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        primary: "#020715",
+        primary: "#003559",
         white: "#ffffff",
         gray: "#f5f5f5",
         red: "#ff0000",
+        pink: "#DA5380",
+        yellow: "#FFCB00",
+        "yellow-hover": "#FFFF8F",
+        "primary-hover": "#2563eb",
         secondary: "#000000",
         accent: "#000000",
       },
+      fontFamily: {
+        prompt: ["Prompt", "sans-serif"],
+        noto: ["Noto Sans KR", "sans-serif"],
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
 } satisfies Config;
