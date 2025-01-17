18f1a436eab1dccef427329af5023fa8d6e6a3df
mini-1018 <anjaemin1018@gmail.com>
2025-01-17 16:18:46 +0900

    :sparkles: [기능구현] 네비게이션바

---
+++ src/widgets/nav/NavButton.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Button from "@/shared/components/buttons/Button";
+
+export default function NavButton() {
+  return (
+    <>
+      <Button content="로그인" onClick={() => {}} />
+    </>
+  );
+}
