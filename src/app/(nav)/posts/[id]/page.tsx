3082b490f60435d9ddc5705e1f39df54aa9d664a
mini-1018 <anjaemin1018@gmail.com>
2025-01-24 17:34:24 +0900

    [fix] 페이지 테스트

--- src/app/(nav)/posts/[id]/page.tsx
+++ src/app/(nav)/posts/[id]/page.tsx
@@ -1,16 +1,16 @@
 import { getPostById } from "@/entities/post/api/post";
 import PostMainContent from "./PostMainContent";
 import { Metadata } from "next";
+import { ResolvingMetadata } from "next";
 
 interface PageParams {
   id: string;
 }
 
-export async function generateMetadata({
-  params,
-}: {
-  params: PageParams;
-}): Promise<Metadata> {
+export async function generateMetadata(
+  { params }: { params: PageParams },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
   const post = await getPostById(params.id);
   return {
     title: post.title,
@@ -23,7 +23,7 @@ export async function generateMetadata({
   };
 }
 
-export default async function Page({ params }: { params: PageParams }) {
-  const post = await getPostById(params.id);
+export default async function Page(props: { params: PageParams }) {
+  const post = await getPostById(props.params.id);
   return <PostMainContent post={post} />;
 }
