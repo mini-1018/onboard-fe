5078ef463e88d200593b3b4d64c6f06131829872
mini-1018 <anjaemin1018@gmail.com>
2025-01-24 17:22:11 +0900

    [fix] 페이지 타입 수정

--- src/app/(nav)/posts/[id]/page.tsx
+++ src/app/(nav)/posts/[id]/page.tsx
@@ -2,15 +2,13 @@ import { getPostById } from "@/entities/post/api/post";
 import PostMainContent from "./PostMainContent";
 import { Metadata } from "next";
 
-type GenerateMetadataProps = {
+type Props = {
   params: { id: string };
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export async function generateMetadata(
-  props: GenerateMetadataProps
-): Promise<Metadata> {
-  const post = await getPostById(props.params.id);
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const post = await getPostById(params.id);
   return {
     title: post.title,
     description: post.content,
@@ -27,9 +25,9 @@ type PageProps = {
   searchParams?: { [key: string]: string | string[] | undefined };
 };
 
-const Post = async (props: PageProps) => {
-  const post = await getPostById(props.params.id);
+async function Page({ params }: PageProps) {
+  const post = await getPostById(params.id);
   return <PostMainContent post={post} />;
-};
+}
 
-export default Post;
+export default Page;
