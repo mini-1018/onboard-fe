95d5b6b485811c6a43482b5b1d20972037ebcf99
mini-1018 <anjaemin1018@gmail.com>
2025-02-25 23:31:47 +0900

    :sparkles: [feat] 포트폴리오

---
+++ src/app/(default)/portfolio/Backend.tsx
@@ -0,0 +1,136 @@
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Backend() {
+  const [activeTab, setActiveTab] = useState("architecture");
+
+  return (
+    <div className="space-y-6 w-full">
+      <h2 className="text-3xl font-bold text-primary border-b border-primary pb-6">
+        백엔드
+      </h2>
+
+      <div className="space-y-6">
+        {/* Tabs */}
+        <div className="flex gap-4">
+          <button
+            onClick={() => setActiveTab("architecture")}
+            className={`px-6 py-3 rounded-lg font-semibold transition-colors
+                ${
+                  activeTab === "architecture"
+                    ? "bg-primary text-white"
+                    : "bg-gray-100 text-gray-600 hover:bg-gray-200"
+                }`}
+          >
+            아키텍처
+          </button>
+          <button
+            onClick={() => setActiveTab("erd")}
+            className={`px-6 py-3 rounded-lg font-semibold transition-colors
+                ${
+                  activeTab === "erd"
+                    ? "bg-primary text-white"
+                    : "bg-gray-100 text-gray-600 hover:bg-gray-200"
+                }`}
+          >
+            ERD
+          </button>
+        </div>
+
+        {/* Content */}
+        {activeTab === "architecture" ? (
+          <div className="relative w-[50%] h-[50vw] 2xl:h-[30vw] bg-white rounded-lg shadow-lg shadow-primary">
+            <Image
+              src="https://mini1018-image.s3.ap-northeast-2.amazonaws.com/profiles/%EC%95%84%ED%82%A4%ED%85%8D%EC%B3%90.png"
+              alt="Architecture"
+              fill
+              className="object-contain p-4"
+            />
+          </div>
+        ) : (
+          <div className="relative w-[50%] h-[50vw] 2xl:h-[30vw] bg-white rounded-lg shadow-lg shadow-primary">
+            <div className="relative w-full h-full">
+              <div className="relative min-w-full min-h-full px-4 group">
+                <Image
+                  src="https://mini1018-image.s3.ap-northeast-2.amazonaws.com/profiles/ERD.svg"
+                  alt="ERD Diagram"
+                  fill
+                  className="object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <ul className="space-y-4 list-disc pl-5">
+        <li>
+          <h3 className="text-xl font-medium">데이터베이스 설계 및 최적화</h3>
+          <p>
+            -스키마 설계 : 3NF를 바탕으로 설계하였고 비키 속성들이 기본 키에
+            완전히 종속되어 이행적 종속이 없도록 구성하였습니다.
+          </p>
+          <p>
+            -인덱싱 : 관계 필드 인덱싱 및 프리스마 어노테이션을 통한 자동
+            인덱싱을 구현하였습니다.
+          </p>
+          <p>
+            -쿼리 최적화 : include 대신 select로 필요한 데이터만 선택하도록
+            하였고 커서 기반 페이지네이션을 적용하였습니다.
+          </p>
+          <p>
+            -캐싱 전략 : 루트 페이지의 게시글 목록을 조회하는 API와 같이 자주
+            조회되는 엔드포인트와 GET 요청에들에 대하여 캐싱하도록
+            구현하였습니다. 특히 자주 조회되는 엔드포인트에서 쿼리 파라미터를
+            사용하는 경우 서비스 레이어에서 쿼리 파라미터에 따른 세부적인 캐싱을
+            하도록 구현하였습니다.
+          </p>
+        </li>
+        <li>
+          <h3 className="text-xl font-medium">트랜잭션</h3>
+          <p>
+            - S3와 DB의 이미지 데이터 무결성을 보장하기 위해 아웃박스 패턴을
+            구현하였습니다. 아웃박스는 이벤트 타입과 페이로드를 통해 메시지를
+            명확히 식별 할 수 있도록 하였으며, 실패 시 재시도 매커니즘을 통해
+            자동 복구가 가능하도록 설계하였습니다.
+          </p>
+          <p>
+            - 데이터 무결성 보장을 위해 레포지토리 레이어에서 트랜잭션 경계를
+            설정하였습니다. 예를들어 사용자 생성 프로세스에서는 사용자 정보와
+            사용자 이미지가 항상 함께 생성되거나 실패하도록 원자적 트랜잭션으로
+            구현하였습니다.
+          </p>
+        </li>
+        <li>
+          <h3 className="text-xl font-medium">에러처리</h3>
+          <p>
+            - 에러를 프론트엔드에서 다루기 편하게 커스텀하였고 에러 상태와 에러
+            코드를 서비스 레이어에서 try catch로 예외 발생 시 throw 하도록
+            하였습니다. 이후 글로벌 에러 핸들러에서 throw 된 에러를 응답으로
+            전송합니다.
+          </p>
+        </li>
+        <li>
+          <h3 className="text-xl font-medium">보안</h3>
+          <p>
+            - Redis를 활용하여 일정 시간동안 과도한 요청 발생 시 차단하고
+            429에러를 클라이언트에게 응답하도록 Rate Limiting을 구현하였습니다.
+          </p>
+          <p>
+            인증이 필요한 엔드포인트의 컨트롤러에 HttpOnly 쿠키에서 JWE 토큰을
+            해독하고 페이로드를 Req에 담아 전달하는 Guard를 설정하였습니다. 이후
+            서비스레이어에서 인가 및 예외를 발생시키도록 구현하였습니다.
+          </p>
+          <p>NginX를 통해 SSL 인증서를 발급 받고 갱신하도록 하였습니다.</p>
+        </li>
+        <li>
+          <h3 className="text-xl font-medium">모니터링 및 로깅</h3>
+          <p>
+            도커를 통한 로그 확인 : 현재 ec2 프리티어의 인스턴스인 t2.micro를
+            사용중입니다. 단일 코어로 성능이 제한적이기 때문에 pm2는 리소스 낭비
+            및 복잡도 증가라 판단하여 도커를 통하여 로그를 확인하고있습니다.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/app/(default)/portfolio/Frontend.tsx
@@ -0,0 +1,126 @@
+import Image from "next/image";
+
+export default function Frontend() {
+  return (
+    <div className="space-y-6 w-full">
+      <h2 className="text-3xl font-bold text-primary border-b border-primary pb-6">
+        프론트엔드
+      </h2>
+      <div className="space-y-6">
+        <ul className="space-y-4 list-disc pl-5">
+          <li>
+            <h3 className="text-xl font-medium">아키텍처</h3>
+            <div className="relative w-[50%] h-[50vw] 2xl:h-[30vw] bg-white rounded-lg shadow-lg shadow-primary">
+              <Image
+                src="https://mini1018-image.s3.ap-northeast-2.amazonaws.com/profiles/%EC%95%84%ED%82%A4%ED%85%8D%EC%B3%90.png"
+                alt="Architecture"
+                fill
+                className="object-contain p-4"
+              />
+            </div>
+          </li>
+          <li>
+            <h3 className="text-xl font-medium">클라이언트-서버 통신 방식</h3>
+            <p>
+              REST API 방식으로 HTTP 메서드를 사용하여 클라이언트와 서버 간의
+              통신을 구현하였습니다.
+            </p>
+          </li>
+          <li>
+            <h3 className="text-xl font-medium">인증 / 보안</h3>
+            <p>
+              - 클라이언트의 모든 요청은 Next Server를 거치도록 설계하였습니다.
+            </p>
+            <p>
+              - 로그인 유지 및 유저 권한이 필요한 API 요청에 필요한 정보를
+              NextAuth JWE로 암호화하여 HttpOnly 쿠키로 전달하도록
+              설계하였습니다.
+            </p>
+          </li>
+          <li>
+            <h3 className="text-xl font-medium">상태관리</h3>
+            <p>- ReactQuery : 서버 상태관리</p>
+            <p>
+              서버 상태 캐싱, 데이터 리프레시, 재요청, 에러 핸들링 등의 서버
+              상태관리를 구현하였습니다.
+            </p>
+            <p>- Zustand : 클라이언트 상태관리</p>
+            <p>전역에서 사용할 Error상태를 구현하였습니다.</p>
+          </li>
+          <li>
+            <h3 className="text-xl font-medium">에러처리</h3>
+            <p>
+              사용자 경험을 위해 서버에서 응답한 에러를 사용자에게 효율적으로
+              제공하기위하여 고민하였고 ReactQuery, Zustand, Toast Ui를 이용하여
+              피드백을 제공하도록 구현하였습니다.
+            </p>
+            <pre className="bg-gray-100 p-4 rounded-lg overflow-x-auto w-[50%]">
+              <code className="text-sm">
+                {`<QueryProvider>
+    <ErrorCatcher />
+    {children}
+    <ToastContainer />
+</QueryProvider>`}
+              </code>
+            </pre>
+            <p>
+              1. API 요청 후 에러가 발생하면 에러의 메시지와 상태코드, path로
+              구성 된 커스텀된 에러 객체를 throw 합니다.
+            </p>
+            <p>
+              2. 쿼리클라이언트(쿼리프로바이더)의 onError를 통해 zustand의 error
+              스토어의 error를 throw된 에러 객체로 업데이트합니다.
+            </p>
+            <p>
+              3. ErrorCatcher는 useEffect를 통해 error 스토어의 error를 구독하고
+              있고 값이 업데이트되면 toast ui에 메시지를 띄워 사용자에게
+              피드백을 제공합니다.
+            </p>
+          </li>
+          <li>
+            <h3 className="text-xl font-medium">모니터링 및 로깅</h3>
+            <p>- Sentry</p>
+            <p>애플리케이션 레벨 에러들을 그룹핑하고 발생 컨텍스트를 분석</p>
+            <p>- Vercel</p>
+            <p>서버 사이드 로깅과 배포, 빌드와 같은 인프라 로깅</p>
+          </li>
+          <li>
+            <h3 className="text-xl font-medium">성능 최적화</h3>
+            <p>
+              Lighthouse를 통해 성능을 테스트하였고 결과로 FCP(첫 번째 컨텐츠
+              페인트), LCP(최대 컨텐츠 페인트) 각각 0.5s 이며 평균 99점의
+              성능지표를 달성하였습니다. 성능 최적화를 위해 다음과 같은 방법을
+              사용하였습니다.
+            </p>
+            <p>
+              - 초기 로딩 속도 향상을 위해 모든 페이지의 초기 데이터를 SSR로
+              제공하였습니다.
+            </p>
+            <p>
+              - 무한스크롤 방식의 페이지네이션을 구현하여 초기 로딩 속도 및
+              사용자 경험을 개선하였습니다.
+            </p>
+            <p>
+              - 코드 스플리팅을 통해 페이지 로드 시 필요한 코드만 로드하도록
+              하였습니다.
+            </p>
+            <p>- Tailwind css 사용하여 반응형 디자인을 구현하였습니다.</p>
+            <p>
+              - 이미지 컴포넌트를 통한 자동 WebP/AVIF 변환, 리사이징, 품질
+              최적화, 지연로딩 등을 구현하였습니다.
+            </p>
+            <p>
+              - JS 공백제거, 변수명 최소화와 같은 자동 압축 및 CDN 캐싱, 해시
+              생성을 Vercel을 통해 최적화 하였습니다.
+            </p>
+            <p>
+              - SSR의 강점을 활용하여 전 페이지에 SEO 메타태그를 적용하였고
+              Robot.txt, site map 등을 통해 google 및 naver에 검색 가능하도록
+              하였습니다.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
